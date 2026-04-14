53762fa907ceec07f3fbe41a71acc40df4016dd3
khyberwear <atifjan2019@gmail.com>
2026-04-14 14:26:04 +0100

    a

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import "./globals.css";
 import Script from "next/script";
 
+const siteUrl = "https://www.instanttyres.co.uk";
+
 export const metadata = {
   title: "Mobile Tyre Fitting Manchester | 24/7 Emergency Service | Call Now",
   description:
@@ -26,7 +28,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 
-        <link rel="canonical" href="https://www.mobiletyrestoday.uk/" />
+        <link rel="canonical" href={siteUrl} />
         
         {/* Preload hero image for LCP */}
         <link rel="preload" as="image" href="https://uploads.webspires.co.uk/mtfnwanees/a.png" />
@@ -34,7 +36,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.mobiletyrestoday.uk/" />
+        <meta property="og:url" content={siteUrl} />
         <meta property="og:image" content="/assets/imgs/optimized/hero-1000.webp" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
